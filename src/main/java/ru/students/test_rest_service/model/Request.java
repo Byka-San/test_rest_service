ea69e440786aeee81274633e0bfdc550ef632602
BykaSan <beasty348@yandex.ru>
2023-05-04 19:34:27 +0500

    Chapter 1

--- src/main/java/ru/students/test_rest_service/model/Request.java
+++ src/main/java/ru/students/test_rest_service/model/Request.java
@@ -3,6 +3,36 @@ package ru.students.test_rest_service.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+
+@Data
+@Builder
 public class Request {
+
+    @NotBlank
+    @Size(max = 32)
+    private String uid;
+
+    @NotBlank
+    @Size (max = 32)
+    private String operationUid;
+
+    private String systemName;
+
+    @NotBlank
+    private String systemTime;
+
+    private String source;
+
+    private Integer communicationId;
+    private Integer templateId;
+
+    @Max(7)
+    private Integer productCode;
+
+    @Max(5)
+    private Integer smsCode;
+
 }
